c13a60418db9f4f60369edfaaea25943bcb1a1e1
surajmaurya0 <surajdevv1@gmail.com>
2023-06-21 11:20:54 +0530

    update

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="has-background-light">
+        <div className="container">
+            <div className="section">
+                <div className="colums">
+                    <div className="column is-3">
+                        <div className="box">
+
+                        </div>
+                    </div>
+                    <div className="column is-9">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
